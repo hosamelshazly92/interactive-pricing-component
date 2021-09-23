2989ab48d0f3a09e22e0ac6dd565f310324f679a
Hosam Elshazly <hosamelshazly92@gmail.com>
2021-09-23 19:44:14 +0200

    edit App component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import {} from "./App.module.css";
+import { container } from "./App.module.css";
 
 function App() {
-  return <div></div>;
+  return <div className={container}></div>;
 }
 
 export default App;
